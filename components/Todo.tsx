545d452bddde0c32ba512344ed59f1b3ec4dc4cf
Misaponce <poncemisael@gmail.com>
2023-09-07 19:45:21 +0100

    added complete and delete icons to each item, both functional

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import React, { useState } from 'react'
+import Image from 'next/image';
 import CustomInput from './CustomInput'
+import CloseIcon from '../assets/images/icon-cross.svg';
+import CheckIcon from '../assets/images/icon-check.svg';
 import { ItemType } from '@/types';
 import { v4 as uuidv4 } from 'uuid';
 
 const Todo = () => {
 
-  const id = uuidv4();
 
   const [todos, setTodos] = useState<ItemType[]>([]);
   const [input, setInput] = useState<string>('');
@@ -18,8 +20,11 @@ const Todo = () => {
 
   const handleSubmitForm = (event: React.FormEvent) => {
     event.preventDefault();
+    setInput('');
 
-    const newTodo: ItemType = {id:id, text:input, completed:false}
+    const newTodo: ItemType = {id: uuidv4(), text:input, completed:false}
+    console.log(newTodo.id)
+    
     setTodos([...todos, newTodo])
   }
 
@@ -27,12 +32,18 @@ const Todo = () => {
   setTodos(
     todos.map((todo) => {
       if (todo.id === id) {
-        return {...todo, completed: !todo.completed};
+        todo.completed = !todo.completed
       }
       return todo;
     })
   )
  }
+
+ const handleDeleteItem = (id:string) => {
+    setTodos(
+      todos.filter((todo) => todo.id !== id)
+    );
+ };
 
   return (
     <div className="todo-wrapper flex flex-col w-full">
@@ -46,7 +57,17 @@ const Todo = () => {
       </form>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id} onClick={() => {handleCompleteItem}} className={`${todo.completed ? 'line-through' : ''}`}>{todo.text}</li>
+          <li key={todo.id} className={`${todo.completed ? 'line-through' : ''} flex justify-between items-center m-4`}>
+            <div onClick={() => {handleCompleteItem(todo.id)}} className='rounded-full border-2 flex justify-center items-center p-1 cursor-pointer'>
+              <Image src={CheckIcon} alt='Check-Icon' className='object-contain w-3 h-3'/>
+            </div>
+            <span className='flex-1 ms-2'>
+              {todo.text}
+            </span>
+            <div onClick={() => handleDeleteItem(todo.id)} className='ml-auto'>
+              <Image src={CloseIcon} alt='Close-icon' className='object-contain w-3 h-3 cursor-pointer'/>
+            </div>
+          </li>
         ))}
       </ul>
       <h4>
